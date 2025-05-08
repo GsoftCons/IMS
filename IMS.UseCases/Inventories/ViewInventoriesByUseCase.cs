b88dcfdf458fea73f2c3f3462d00839219f23494
GSoft Consulting <s.gaboli@gsoftconsulting.it>
2025-05-08 14:44:17 +0200

    Initial commit

---
+++ IMS.UseCases/Inventories/ViewInventoriesByUseCase.cs
@@ -0,0 +1,27 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoriesByUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+
+        public ViewInventoriesByUseCase(IInventoryRepository invetoryRepository)
+        {
+            this.inventoryRepository = invetoryRepository;
+        }
+
+
+
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name= "")
+        {
+            return await inventoryRepository.GetInventoriesByNameAsync(name);
+        }
+    }
+}
